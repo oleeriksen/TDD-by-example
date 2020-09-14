f7672d7da1a4936222686a3fa81b989727d512ae
oleeriksen <oe@easv.dk>
2020-09-14 11:42:58 +0200

    init

---
+++ ProductionCode/ArrayFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ProductionCode
+{
+    public class ArrayFunctions
+    {
+        public ArrayFunctions()
+        {
+        }
+
+        // return true iff all values in a occurs only once.
+        // must throw an exception if a is null
+        public bool IsUnique(int[] a)
+        {
+            if (a == null)
+                throw new Exception();
+            for (int i = 1; i < a.Length; i++)
+                if (a.Count(x => x == a[i]) > 1)
+                    return false;
+            return true;
+        }
+
+
+    }
+}
---
+++ TestProductionCode/UnitTest1.cs
@@ -0,0 +1,78 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductionCode;
+
+namespace TestProductionCode
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ArrayFunctions af = new ArrayFunctions();
+
+            int[] a = { 1, 2 };
+            Assert.IsTrue(af.IsUnique(a));
+
+            int[] b = { };
+            Assert.IsTrue(af.IsUnique(b));
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            ArrayFunctions af = new ArrayFunctions();
+
+            int[] a = { 1, 1 };
+            Assert.IsFalse(af.IsUnique(a));
+
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            ArrayFunctions af = new ArrayFunctions();
+
+            int[] a = { 1, 2, 1 };
+            Assert.IsFalse(af.IsUnique(a));
+
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            ArrayFunctions af = new ArrayFunctions();
+
+            int[] a = { 1, 2, 1 };
+            Assert.IsFalse(af.IsUnique(a));
+
+
+        }
+
+        [TestMethod]
+        public void TestException()
+        {
+            ArrayFunctions af = new ArrayFunctions();
+
+            Assert.ThrowsException<Exception>(() => af.IsUnique(null));
+
+        }
+
+        [TestMethod]
+        public void TestPerformance()
+        {
+            int N = 1000000000;
+            ArrayFunctions af = new ArrayFunctions();
+            Random r = new Random();
+            int[] a = new int[N];
+            for (int i = 0; i < a.Length; i++) a[i] = r.Next();
+            Assert.IsFalse(af.IsUnique(a));
+
+        }
+    }
+}
