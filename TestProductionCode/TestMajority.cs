1e5adf8f73747b2db7eea01c13bfff8caa0bfb39
oleeriksen <oe@easv.dk>
2020-09-21 10:05:51 +0200

    Update TestMajority.cs

--- TestProductionCode/TestMajority.cs
+++ TestProductionCode/TestMajority.cs
@@ -11,10 +11,19 @@ namespace TestProductionCode
         [TestMethod]
         public void TestFalse()
         {
+            // a) create object
             Majority m = new Majority();
-            int[] input = { 1, 2, 3, 1, 2, 3 };
 
-            Assert.IsFalse(m.HasMajority(input));
+            // b) setup
+            int[] input = { 1, 2, 3, 1, 2, 3 };
+            bool expectedResult = false;
+            bool actualResult;
+
+            // c) act
+            actualResult = m.HasMajority(input);
+
+            // d) assert
+            Assert.AreEqual(actualResult, expectedResult);
         }
 
         [TestMethod]
@@ -48,12 +57,17 @@ namespace TestProductionCode
         [Timeout(4000)]
         public void TestPerformance()
         {
+            // a) create objects
             int N = 100000000;
             Majority m = new Majority();
+
+            // b) setup
             int[] input = new int[N];
             Random r = new Random();
             for (int i = 0; i < input.Length; i++)
                 input[i] = r.Next(100);
+
+            // c) act
             m.HasMajority(input);
         }
     }
